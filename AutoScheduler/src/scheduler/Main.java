e38ac7e682f0eb7766a54b6ecef3473020a38269
denuccisv22 <36933843+denuccisv22@users.noreply.github.com>
2019-11-07 14:49:41 -0600

    Add files via upload

---
+++ AutoScheduler/src/scheduler/Main.java
@@ -0,0 +1,11 @@
+package scheduler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shift twelve = new Shift("twelve", 7);
+		Employee e1 = new Employee("Sam", twelve, 14, true);
+	}
+
+}
